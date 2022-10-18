6b54048a6b156d75c9507fb2aec5efc6a6817212
cognivore <noreply>
2022-10-18 16:43:40 +0100

    Some float unops semantics.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,97 @@
-use wasmer::{Store, Module, Instance, Value, imports};
+use std::panic;
+
+use wasmer::{imports, Instance, Module, Store, Value};
 
 fn main() -> anyhow::Result<()> {
+    // let module_wat = r#"
+    // (module
+    //   (type $t0 (func (param i32) (result i32)))
+    //   (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+    //     local.get $p0
+    //     i32.const 1
+    //     i32.add))
+    // "#;
+
     let module_wat = r#"
     (module
-      (type $t0 (func (param i32) (result i32)))
-      (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-        get_local $p0
-        i32.const 1
-        i32.add))
+
+      (func $f (export "f") (param $x f32) (result v128)
+        (local $a f32) (local $b f32) (local $c f32) (local $d f32) (local $y v128)
+
+        f32.const 0
+        f32x4.splat
+        local.set $y
+
+        local.get $x
+        f32.neg
+        local.set $a
+
+        local.get $x
+        f32.nearest
+        local.set $b
+
+        local.get $x
+        f32.trunc
+        local.set $c
+
+        local.get $x
+        f32.ceil
+        local.set $d
+
+        local.get $y
+        local.get $a
+        f32x4.replace_lane 0
+        local.set $y
+
+        local.get $y
+        local.get $b
+        f32x4.replace_lane 1
+        local.set $y
+
+        local.get $y
+        local.get $c
+        f32x4.replace_lane 2
+        local.set $y
+
+        local.get $y
+        local.get $d
+        f32x4.replace_lane 3
+        local.set $y
+
+        local.get $y
+        return
+
+      )
+
+      (func $f32_unv (export "f32.unv") (param $x v128) (param $i i32) (result f32)
+        ;; f32x4.extract_lane 0
+        (block
+          (block
+            (block
+              (block
+                (block (local.get $i)
+                       (br_table 0 1 2 3 4)
+                )
+                local.get $x
+                f32x4.extract_lane 0
+                return
+              )
+              local.get $x
+              f32x4.extract_lane 1
+              return
+            )
+            local.get $x
+            f32x4.extract_lane 2
+            return
+          )
+          local.get $x
+          f32x4.extract_lane 3
+          return
+        )
+        f32.const nan
+        return
+      )
+    )
     "#;
 
     let store = Store::default();
@@ -16,9 +100,36 @@ fn main() -> anyhow::Result<()> {
     let import_object = imports! {};
     let instance = Instance::new(&module, &import_object)?;
 
-    let add_one = instance.exports.get_function("add_one")?;
-    let result = add_one.call(&[Value::I32(42)])?;
-    assert_eq!(result[0], Value::I32(43));
+    let f = instance.exports.get_function("f")?;
+    let f32_unv = instance.exports.get_function("f32.unv")?;
+
+    let result = f.call(&[Value::F32(3.1415926)])?;
+
+    let x0 = match result[0] {
+        Value::V128(x) => f32_unv.call(&[Value::V128(x), Value::I32(0)]),
+        _ => panic!(),
+    };
+
+    let x1 = match result[0] {
+        Value::V128(x) => f32_unv.call(&[Value::V128(x), Value::I32(1)]),
+        _ => panic!(),
+    };
+
+    let x2 = match result[0] {
+        Value::V128(x) => f32_unv.call(&[Value::V128(x), Value::I32(2)]),
+        _ => panic!(),
+    };
+
+    let x3 = match result[0] {
+        Value::V128(x) => f32_unv.call(&[Value::V128(x), Value::I32(3)]),
+        _ => panic!(),
+    };
+
+    assert_eq!(x0.unwrap()[0], Value::F32(-3.1415926));
+    // https://github.com/WebAssembly/gc-js-customization/blob/8dfabda9b7925543dcb9afe1fda7d5038374dbdd/test/core/float_misc.wast#L647
+    assert_eq!(x1.unwrap()[0], Value::F32(3.00));
+    assert_eq!(x2.unwrap()[0], Value::F32(3.00));
+    assert_eq!(x3.unwrap()[0], Value::F32(4.00));
 
     Ok(())
 }
